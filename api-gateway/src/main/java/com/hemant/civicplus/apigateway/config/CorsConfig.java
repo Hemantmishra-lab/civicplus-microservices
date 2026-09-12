2a8c195d442d0fc361bb66fc2d665a3ad9df414f
Hemantmishra-lab <hemantmishra19781978@gmail.com>
2026-09-12 19:12:19 +0530

    Fix: API's CORS Configuration

--- api-gateway/src/main/java/com/hemant/civicplus/apigateway/config/CorsConfig.java
+++ api-gateway/src/main/java/com/hemant/civicplus/apigateway/config/CorsConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 public class CorsConfig {
@@ -14,7 +15,13 @@ public class CorsConfig {
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173", "http://localhost:5174"));
+
+        corsConfig.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000",
+                "http://13.210.175.10:3000",
+                "http://13.210.175.10"
+        ));
+
         corsConfig.setMaxAge(3600L);
         corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         corsConfig.addAllowedHeader("*");
